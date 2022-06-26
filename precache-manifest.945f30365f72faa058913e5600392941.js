c4ed59eed68e97c0cac88229a16b419422bd2769
Junaid <zulfiqar.junaid95@gmail.com>
2022-06-25 18:49:18 -0700

    Updates

---
+++ precache-manifest.945f30365f72faa058913e5600392941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7e385d4dacdf1a2d064843d9ca203f",
+    "url": "/Color-Generator/index.html"
+  },
+  {
+    "revision": "27533718fb27956d89f8",
+    "url": "/Color-Generator/static/css/main.5b6ce432.chunk.css"
+  },
+  {
+    "revision": "fc90f6e8653c1e9fd123",
+    "url": "/Color-Generator/static/js/2.d4cef545.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/Color-Generator/static/js/2.d4cef545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27533718fb27956d89f8",
+    "url": "/Color-Generator/static/js/main.2e5af1c5.chunk.js"
+  },
+  {
+    "revision": "c5bb234307e7459e241b",
+    "url": "/Color-Generator/static/js/runtime-main.26acfe79.js"
+  }
+]);
